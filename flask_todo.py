77f1a86f3d20a3ce896da350bc53843cfda336d2
Johnmarare <mararejohn@gmail.com>
2023-12-22 13:07:08 +0300

    database config

--- flask_todo.py
+++ flask_todo.py
@@ -1,7 +1,33 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '05c9681678fa66c1ccbbe721caedcc8b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:marare@localhost/db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    tasks = db.relationship('Task', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_tasked = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Task('{self.title}', '{self.date_tasked}', '{self.content}')"
 
 posts = [
     {
